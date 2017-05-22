466b2534e78446f2b39b2640bca5b5e9928bee45
ivyxuan <iloveivyxuan@gmail.com>
2017-05-23 05:08:58 +0800

    third

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,3 +1,10 @@
+class Bowling {
+    public static void main(String []args){
+        BowlingGame game = new BowlingGame("X|7/|9-|X|-8|8/|-6|X|X|X||81");
+        System.out.println(game.score());
+    }
+}
+
 class FrameScore {
     public int scoreOne;
     public int scoreTwo;
@@ -6,11 +13,11 @@ class FrameScore {
 
     public FrameScore(String str) {
         str = str.replaceAll("-", "0");
-        if(!!(str.contains("X"))) {
+        if(str.contains("X")) {
             this.scoreOne = 10;
             this.scoreTwo = 0;
             this.type = "Strick";
-        } else if (!!(str.contains("/"))) {
+        } else if (str.contains("/")) {
             String[] scores = str.split("");
             this.scoreOne = Integer.parseInt(scores[0]);
             this.scoreTwo = 10 - Integer.parseInt(scores[0]);
@@ -29,18 +36,34 @@ class FrameScore {
     public int score() {
         return this.scoreOne + this.scoreTwo + this.addition;
     }
+
+    public void printSelf() {
+        System.out.println(this.scoreOne + ", " + this.scoreTwo + ", " + this.addition);
+    }
 }
 
 public class BowlingGame {
     String[] addition = new String[2];
     FrameScore[] frameScores = new FrameScore[10];
     FrameScore[] calcScores = new FrameScore[12];
-
     public BowlingGame(String scoreString) {
-        String[] scores = scoreString.split("\\|\\|")[0].split("\\|");
-        String[] additionString = scoreString.split("\\|\\|")[1].split("");
-        this.addition[0] = additionString[0] + "00";
-        this.addition[1] = additionString[1] + "00";
+        String[] splitString = scoreString.split("\\|\\|");
+        String[] scores = splitString[0].split("\\|");
+        String additionStr;
+        if(splitString.length > 1) {
+            additionStr = splitString[1];
+        } else {
+            additionStr = "00";
+        }
+        switch (additionStr.length()) {
+            case 0 :
+                additionStr = "00";
+            case 1 :
+                additionStr += "0";
+        }
+        String[] additionString = additionStr.split("");
+        this.addition[0] = additionString[0] + "0";
+        this.addition[1] = additionString[1] + "0";
 
         for(int i = 0; i < 10; i += 1) {
             this.frameScores[i] = new FrameScore(scores[i]);
@@ -51,9 +74,9 @@ public class BowlingGame {
 
         for(int i = 0; i < 10; i += 1) {
             FrameScore fScr = this.frameScores[i];
-            if(fScr.type == "Strike") {
+            if(fScr.type == "Strick") {
                 fScr.addition += this.calcScores[i + 1].rawScore();
-                if(this.calcScores[i + 1].type == "Strike") {
+                if(this.calcScores[i + 1].type == "Strick") {
                     fScr.addition += this.calcScores[i + 2].scoreOne;
                 }
             } else if(fScr.type == "Spare") {
@@ -66,7 +89,10 @@ public class BowlingGame {
         int sum = 0;
         for(int i = 0; i < 10; i += 1) {
             sum += this.frameScores[i].score();
+            this.frameScores[i].printSelf();
         }
         return sum;
     }
+
+
 }
