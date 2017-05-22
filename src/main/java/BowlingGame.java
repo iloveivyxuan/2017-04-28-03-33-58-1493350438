9b7de9cde8ee75acbc4b410073a363ecdfb036a2
ivyxuan <iloveivyxuan@gmail.com>
2017-05-22 22:35:20 +0800

    second

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,65 +1,47 @@
 public class BowlingGame {
-
-    public int getBowlingScore(String bowlingCode) {       
-        //System.out.println(recordsArray);
-
-        String[] recordsArray = bowlingCode.split("|");
-        int[] extend = new int[22];
-
-        int scores = 0;
+    public int[] pt = new int[24];
+    public int findNot(int x){
+        for(int i=x;;i++) if(pt[i]!=-1) return i;
+    }
+    public int getBowlingScore(String bowlingCode) {
+        int sum = 0;
+        for (int i = 0; i < 22; i++)
+            pt[i] = 0;
         int len = bowlingCode.length();
-        int[] frameScore = new int[10];
-
-        for (int charIndex = 0, framIndex = 0, IndexInFrame = 1, extendIndex = 0; charIndex < len; charIndex ++) {
-            switch (bowlingCode.charAt(charIndex)) {
-                case '|':
-                    charIndex += 1;
-                    framIndex += 1;
-                    IndexInFrame = 1;
-                    break;
+        for (int i = 0, j = 0, k = 0; j < len; j++) {
+            switch (bowlingCode.charAt(j)) {
                 case 'X':
-                    frameScore[framIndex] = 10;
-                    extend[extendIndex] = charIndex + 1;
-                    extendIndex += 1;
-                    extend[extendIndex] = charIndex + 2;
-                    extendIndex += 1;
-                    charIndex += 1;
-                case '-':
-                    charIndex += 1;
-                    IndexInFrame += 1;
+                    pt[i + k] = 10;
+                    pt[i + k + 1]=-1;
+                    break;
                 case '/':
-                    frameScore[framIndex] = 10;
-                    extend[extendIndex] = charIndex + 1;
-                    extendIndex += 1;
-                    charIndex += 1;
+                    pt[i + k] = 10 - pt[i];
+                    break;
+                case '-':
+                    pt[i + k] = 0;
+                    k++;
+                    break;
+                case '|':
+                    if(i<20)
+                        i += 2;
+                    k = 0;
+                    break;
                 default:
-                    if (IndexInFrame == 1) {
-                        frameScore[framIndex] = bowlingCode.charAt(charIndex) - '0';
-                    } else {
-                        frameScore[framIndex] += bowlingCode.charAt(charIndex) - '0';
-                    }
-                    IndexInFrame += 1;
-                    charIndex += 1;
-
+                    pt[i + k] = bowlingCode.charAt(j) - '0';
+                    k++;
+                    break;
             }
         }
-
-        for (int i = 0; i < 10; i++) {
-            scores = scores + frameScore[i];
-        }
-
-        for (int i = 0; i < 22; i++) {
-            if (bowlingCode.charAt(extend[i]) == 'X') {
-                scores = scores + 10; 
-            } else if (bowlingCode.charAt(extend[i]) == '-') {
-                scores = scores + 0;
-            } else if (bowlingCode.charAt(extend[i]) == '/') {
-                scores = scores + 10 - bowlingCode.charAt(extend[i] - 1);
-            } else {
-                scores = scores + (bowlingCode.charAt(i) - '0');
+        for(int i=0;i<10;i++){
+            if(pt[i*2]==10){
+                int t=findNot(i*2+1);
+                sum=sum+pt[i*2]+pt[findNot(t+1)]+pt[t];
+            }else if(pt[i*2]+pt[i*2+1]==10){
+                sum=sum+10+pt[findNot((i+1)*2)];
+            }else{
+                sum=sum+pt[i*2]+pt[i*2+1];
             }
         }
-
-        return scores;
+        return sum;
     }
 }
