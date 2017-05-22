1eb9afcde714b00e4052bde6e9ea657f3eb11d96
ivyxuan <iloveivyxuan@gmail.com>
2017-05-23 05:28:14 +0800

    third

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,169 +1,98 @@
-import java.util.HashMap;
-import java.util.Map;
+class Bowling {
+    public static void main(String []args){
+        BowlingGame game = new BowlingGame("X|7/|9-|X|-8|8/|-6|X|X|X||81");
+        System.out.println(game.score());
+    }
+}
+
+class FrameScore {
+    public int scoreOne;
+    public int scoreTwo;
+    public int addition = 0;
+    public String type = "Default";
+
+    public FrameScore(String str) {
+        str = str.replaceAll("-", "0");
+        if(str.contains("X")) {
+            this.scoreOne = 10;
+            this.scoreTwo = 0;
+            this.type = "Strick";
+        } else if (str.contains("/")) {
+            String[] scores = str.split("");
+            this.scoreOne = Integer.parseInt(scores[0]);
+            this.scoreTwo = 10 - Integer.parseInt(scores[0]);
+            this.type = "Spare";
+        } else {
+            String[] scores = str.split("");
+            this.scoreOne = Integer.parseInt(scores[0]);
+            this.scoreTwo = Integer.parseInt(scores[1]);
+        }
+    }
+
+    public int rawScore() {
+        return this.scoreOne + this.scoreTwo;
+    }
+
+    public int score() {
+        return this.scoreOne + this.scoreTwo + this.addition;
+    }
+
+    public void printSelf() {
+        System.out.println(this.scoreOne + ", " + this.scoreTwo + ", " + this.addition);
+    }
+}
 
 public class BowlingGame {
-    Map<Character, Integer> score = new HashMap<Character, Integer>();
-    public int getBowlingScore(String bowlingCode) {
-        score.put('X', 10);
-        score.put('-', 0);
-        String[] tenBowlingCode = bowlingCode.split("\\|\\|")[0].split("\\|");
-        String extraBowling = bowlingCode.split("\\|\\|").length == 2 ? bowlingCode.split("\\|\\|")[1] : null;
-        Integer totalScore = 0;
-
-        for (int index = 0; index < tenBowlingCode.length; ++index) {
-            if (index < 8) {
-                totalScore += getFrameScoreBeforeEighthFrame(index, tenBowlingCode);
-            } else if (index == 8) {
-                totalScore += getFrameScoreEqualEighthFrame(index, tenBowlingCode, extraBowling);
-            } else {
-                totalScore += getFrameScoreEqualNinthFrame(index, tenBowlingCode, extraBowling);
-            }
-        }
-
-        return totalScore;
-    }
-
-    public Integer getFrameScoreBeforeEighthFrame(Integer index, String[] tenBowlingCode) {
-
-        //index frame strike
-        if (tenBowlingCode[index].charAt(0) == 'X') {
-            if (tenBowlingCode[index + 1].charAt(0) == 'X') { //index+1 frame strike
-                if (tenBowlingCode[index + 2].charAt(0) == 'X') { //index+2 frame first code == 'X' strike
-
-                    return 30;
-                } else if (tenBowlingCode[index + 2].charAt(0) == '-') { //index+2 frame first code == '-' miss
-
-                    return 20;
-                } else {
-
-                    return 20 + Integer.parseInt(String.valueOf(tenBowlingCode[index + 2].charAt(0))); ////index+2 frame other code
-                }
-            } else if (tenBowlingCode[index + 1].charAt(1) == '/') { //index+1 frame spare
-
-                return 20;
-            } else {
-                boolean isEqual = "-".equals(String.valueOf(tenBowlingCode[index + 1].charAt(0)));
-                return 10 + (isEqual == true ? Integer.parseInt(String.valueOf(tenBowlingCode[index + 1].charAt(1)))
-                        : Integer.parseInt(String.valueOf(tenBowlingCode[index + 1].charAt(0))));
-            }
-        }
-
-        //index frame spare
-        if (tenBowlingCode[index].charAt(1) == '/') {
-            if (tenBowlingCode[index + 1].charAt(0) == 'X') { //index+1 frame first code == 'X' strike
-
-                return 20;
-            } else if (tenBowlingCode[index + 1].charAt(0) == '-') { //index+1 frame first code == '-' miss
-
-                return 10;
-            } else {
-
-                return 10 + Integer.parseInt(String.valueOf(tenBowlingCode[index + 1].charAt(0))); ////index+2 frame other code
-            }
-        }
-
-        //index frame  no spare or strike
-        if (tenBowlingCode[index].charAt(0) == '-') {
-
-            return Integer.parseInt(String.valueOf(tenBowlingCode[index].charAt(1)));
+    String[] addition = new String[2];
+    FrameScore[] frameScores = new FrameScore[10];
+    FrameScore[] calcScores = new FrameScore[12];
+    public BowlingGame(String scoreString) {
+        String[] splitString = scoreString.split("\\|\\|");
+        String[] scores = splitString[0].split("\\|");
+        String additionStr;
+        if(splitString.length > 1) {
+            additionStr = splitString[1];
         } else {
-
-            return Integer.parseInt(String.valueOf(tenBowlingCode[index].charAt(0)));
+            additionStr = "00";
         }
+        switch (additionStr.length()) {
+            case 0 :
+                additionStr = "00";
+            case 1 :
+                additionStr += "0";
+        }
+        String[] additionString = additionStr.split("");
+        this.addition[0] = additionString[0] + "0";
+        this.addition[1] = additionString[1] + "0";
 
-    }
+        for(int i = 0; i < 10; i += 1) {
+            this.frameScores[i] = new FrameScore(scores[i]);
+            this.calcScores[i] = new FrameScore(scores[i]);
+        }
+        this.calcScores[10] = new FrameScore(this.addition[0]);
+        this.calcScores[11] = new FrameScore(this.addition[1]);
 
-    public Integer getFrameScoreEqualEighthFrame(Integer index, String[] tenBowlingCode, String extraBowling) {
-
-        //index frame strike
-        if (tenBowlingCode[index].charAt(0) == 'X') {
-            if (tenBowlingCode[index + 1].charAt(0) == 'X') { //index+1 frame strike
-                if (extraBowling.charAt(0) == 'X') { //index+2 frame first code == 'X' strike
-
-                    return 30;
-                } else if (extraBowling.charAt(0) == '-') { //index+2 frame first code == '-' miss
-
-                    return 20;
-                } else {
-
-                    return 20 + Integer.parseInt(String.valueOf(extraBowling.charAt(0))); ////index+2 frame other code
+        for(int i = 0; i < 10; i += 1) {
+            FrameScore fScr = this.frameScores[i];
+            if(fScr.type == "Strick") {
+                fScr.addition += this.calcScores[i + 1].rawScore();
+                if(this.calcScores[i + 1].type == "Strick") {
+                    fScr.addition += this.calcScores[i + 2].scoreOne;
                 }
-            } else if (tenBowlingCode[index + 1].charAt(1) == '/') { //index+1 frame spare
-
-                return 20;
-            } else {
-
-                return 10 + tenBowlingCode[index + 1].charAt(0) == '-'
-                        ? Integer.parseInt(String.valueOf(tenBowlingCode[index + 1].charAt(1)))
-                        : Integer.parseInt(String.valueOf(tenBowlingCode[index + 1].charAt(0)));
+            } else if(fScr.type == "Spare") {
+                fScr.addition += this.calcScores[i + 1].scoreOne;
             }
-        }
-
-        //index frame spare
-        if (tenBowlingCode[index].charAt(1) == '/') {
-            if (tenBowlingCode[index + 1].charAt(0) == 'X') { //index+1 frame first code == 'X' strike
-
-                return 20;
-            } else if (tenBowlingCode[index + 1].charAt(0) == '-') { //index+1 frame first code == '-' miss
-
-                return 10;
-            } else {
-
-                return 10 + Integer.parseInt(String.valueOf(tenBowlingCode[index + 1].charAt(0))); ////index+2 frame other code
-            }
-        }
-
-        //index frame  no spare or strike
-        if (tenBowlingCode[index].charAt(0) == '-') {
-
-            return Integer.parseInt(String.valueOf(tenBowlingCode[index].charAt(1)));
-        } else {
-
-            return Integer.parseInt(String.valueOf(tenBowlingCode[index].charAt(0)));
         }
     }
 
-    public Integer getFrameScoreEqualNinthFrame(Integer index, String[] tenBowlingCode, String extraBowling) {
-        Integer frameScore = 0;
-
-        //index frame strike
-        if (tenBowlingCode[index].charAt(0) == 'X') {
-            if (score.containsKey(extraBowling.charAt(0))) {
-                frameScore += score.get(extraBowling.charAt(0));
-            }else{
-                frameScore += Integer.parseInt(String.valueOf(extraBowling.charAt(0)));
-            }
-            if (score.containsKey(extraBowling.charAt(1))) {
-                frameScore += score.get(extraBowling.charAt(1));
-            }else{
-                frameScore += Integer.parseInt(String.valueOf(extraBowling.charAt(1)));
-            }
-            return frameScore + 10;
+    public int score() {
+        int sum = 0;
+        for(int i = 0; i < 10; i += 1) {
+            sum += this.frameScores[i].score();
+            this.frameScores[i].printSelf();
         }
-
-        //index frame spare
-        if (tenBowlingCode[index].charAt(1) == '/') {
-            if (score.containsKey(extraBowling.charAt(0))) {
-                frameScore += score.get(extraBowling.charAt(0));
-            }else{
-                frameScore += Integer.parseInt(String.valueOf(extraBowling.charAt(0)));
-            }
-
-            return frameScore + 10;
-        }
-
-        //index frame not spare or strike
-        if (tenBowlingCode[index].contains("-")) {
-            if (score.containsKey(tenBowlingCode[index].charAt(0))) {
-                frameScore += score.get(tenBowlingCode[index].charAt(0));
-            } else {
-                frameScore += Integer.parseInt(String.valueOf(tenBowlingCode[index].charAt(0)));
-            }
-
-            return frameScore;
-        }
-
-        return 0;
+        return sum;
     }
+
 
 }
